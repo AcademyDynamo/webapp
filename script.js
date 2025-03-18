632d3b47ab8641e4950e914b38a76c9e33eea186
AcademyDynamo <artist7064@gmail.com>
2025-03-19 00:39:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const players = {
+    const playersData = {
         goalkeeper: [
             { name: "Алисон", team: "Ливерпуль", logo: "images/logos/liverpool.png" },
-            { name: "Нойер", team: "Бавария", logo: "images/logos/bayern.png" },
             { name: "Куртуа", team: "Реал", logo: "images/logos/real_madrid.png" }
         ],
         defender: [
             { name: "Рамос", team: "ПСЖ", logo: "images/logos/psg.png" },
-            { name: "Ван Дейк", team: "Ливерпуль", logo: "images/logos/liverpool.png" },
-            { name: "Кимпембе", team: "ПСЖ", logo: "images/logos/psg.png" }
+            { name: "Ван Дейк", team: "Ливерпуль", logo: "images/logos/liverpool.png" }
         ],
         midfielder: [
             { name: "Де Брюйне", team: "Манчестер Сити", logo: "images/logos/mancity.png" },
-            { name: "Кроос", team: "Реал", logo: "images/logos/real_madrid.png" },
-            { name: "Модрич", team: "Реал", logo: "images/logos/real_madrid.png" }
+            { name: "Кроос", team: "Реал", logo: "images/logos/real_madrid.png" }
         ],
         forward: [
             { name: "Месси", team: "Интер Майами", logo: "images/logos/inter_miami.png" },
-            { name: "Роналду", team: "Аль-Наср", logo: "images/logos/al_nassr.png" },
             { name: "Холанд", team: "Манчестер Сити", logo: "images/logos/mancity.png" }
         ]
     };
 
+    const playersListContainer = document.querySelector(".players");
     const playersList = document.getElementById("playersList");
 
     document.querySelectorAll(".shirt").forEach(shirt => {
@@ -33,10 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function showPlayers(position) {
         playersList.innerHTML = "";
-        players[position].forEach(player => {
+        playersData[position].forEach(player => {
             const li = document.createElement("li");
             li.innerHTML = <img src="${player.logo}" alt="${player.team}"> ${player.name} (${player.team});
             playersList.appendChild(li);
         });
+
+        playersListContainer.style.display = "block";
     }
 });
