b3aa61ea46ee4045b090c9401a801ad99248be06
AcademyDynamo <artist7064@gmail.com>
2025-03-19 01:14:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const playersData = {
-        goalkeeper: [
-            { name: "Алисон", team: "Ливерпуль", logo: "images/logos/liverpool.png", shirt: "images/shirts/liverpool.png" },
-            { name: "Куртуа", team: "Реал", logo: "images/logos/real_madrid.png", shirt: "images/shirts/real.png" }
-        ],
-        defender: [
-            { name: "Рамос", team: "ПСЖ", logo: "images/logos/psg.png", shirt: "images/shirts/psg.png" },
-            { name: "Ван Дейк", team: "Ливерпуль", logo: "images/logos/liverpool.png", shirt: "images/shirts/liverpool.png" }
-        ],
-        midfielder: [
-            { name: "Де Брюйне", team: "Манчестер Сити", logo: "images/logos/mancity.png", shirt: "images/shirts/mancity.png" },
-            { name: "Кроос", team: "Реал", logo: "images/logos/real_madrid.png", shirt: "images/shirts/real.png" }
-        ],
         forward: [
-            { name: "Месси", team: "Интер Майами", logo: "images/logos/inter_miami.png", shirt: "images/shirts/inter_miami.png" },
-            { name: "Холанд", team: "Манчестер Сити", logo: "images/logos/mancity.png", shirt: "images/shirts/mancity.png" }
+            { name: "Месси", team: "Интер Майами", logo: "images/1football-field.png" },
+            { name: "Холанд", team: "Манчестер Сити", logo: "images/1football-field.png" },
+            { name: "Мбаппе", team: "ПСЖ", logo: "images/1football-field.png" }
         ]
     };
 
@@ -22,12 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const playersListContainer = document.querySelector(".players");
     const playersList = document.getElementById("playersList");
 
-    document.querySelectorAll(".shirt").forEach(shirt => {
-        shirt.addEventListener("click", function () {
-            selectedShirt = this;
-            const position = this.dataset.position;
-            showPlayers(position);
-        });
+    document.querySelector(".shirt").addEventListener("click", function () {
+        selectedShirt = this;
+        showPlayers("forward");
     });
 
     function showPlayers(position) {
@@ -44,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function selectPlayer(player) {
         if (selectedShirt) {
-            selectedShirt.querySelector("img").src = player.shirt;
             selectedShirt.querySelector(".player-name").textContent = player.name;
         }
         playersListContainer.style.display = "none";
